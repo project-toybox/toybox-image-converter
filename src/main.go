20f069d8483c4260c712a5379af05bff6c0529e5
June <hexstudio16@gmail.com>
2022-09-11 14:27:15 +0900

    Add signature text on startup

--- src/main.go
+++ src/main.go
@@ -9,6 +9,14 @@ import (
 )
 
 func main() {
+	var signature string = `
+ _____         _               ___ ___ ___ 
+|_   _|__ _  _| |__  _____ __ |_ _/ __/ __|
+  | |/ _ \ || | '_ \/ _ \ \ /  | | (__\__ \
+  |_|\___/\_, |_.__/\___/_\_\ |___\___|___/
+	  |__/                             `;
+
+	fmt.Println(signature);
 	fmt.Println("Toybox Image Conversion Server");
 	fmt.Println("Copyright (c) 2022 Project Toybox all rights reserved.");
 
